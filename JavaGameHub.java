46f88aa6910ecae6133c56236d9d6a2b354bdb9c
Ben Edwards <benji.edwards60@gmail.com>
2020-07-16 09:36:30 +0100

    Add files via upload

---
+++ JavaGameHub.java
@@ -0,0 +1,19 @@
+package javagamehub;
+
+/**
+ *
+ * @author Ben Edwards
+ */
+public class JavaGameHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu main = new MainMenu();
+        main.setVisible(true);
+        main.setSize(800, 600);
+        main.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Pong.java
@@ -0,0 +1,36 @@
+package javagamehub;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Ben Edwards
+ */
+public class Pong extends JFrame 
+{
+    private final static int WIDTH = 700, HEIGHT = 450;
+    private PongPanel panel;
+
+    public Pong() 
+    {
+        setSize(WIDTH, HEIGHT);
+        setTitle("Pong");
+        setBackground(Color.WHITE);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new PongPanel(this);
+        add(panel);
+    }
+
+    public PongPanel getPanel() 
+    {
+        return panel;
+    }
+
+    public static void main(String[] args) 
+    {
+        new Pong();
+    }
+}
